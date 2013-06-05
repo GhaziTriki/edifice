6c60eb8bc4e8831d88b0d9c7ead1a5451d505bd1
Ghazi Triki <ghazi.nocturne@gmail.com>
2013-06-05 10:17:30 +0100

    Created edifice Facade.

---
+++ src/Lionart/Edifice/Support/Facades/Edifice.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Ghazi
+ * Date: 05/06/13
+ * Time: 10:13
+ */
+
+namespace Lionart\Edifice\Support\Facades;
+
+class Edifice extends Facade {
+
+	/**
+	 * Get the registered name of the component.
+	 *
+	 * @return string
+	 */
+	protected static function getFacadeAccessor() {
+		return 'edifice';
+	}
+}
