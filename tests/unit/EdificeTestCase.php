8887cedb6fbeb18451446d207500c9c38aa30900
Ghazi Triki <ghazi.nocturne@gmail.com>
2013-06-09 15:52:40 +0100

    Updated base test class.

--- tests/unit/EdificeTestCase.php
+++ tests/unit/EdificeTestCase.php
@@ -1,8 +1,10 @@
 <?php
+
 use Illuminate\Html\FormBuilder;
 use Illuminate\Html\HtmlBuilder;
 use Illuminate\Http\Request;
 use Illuminate\Routing\UrlGenerator;
+use Illuminate\Session\Store;
 use Lionart\Edifice\Form\EdificeForm;
 use Mockery as m;
 use Symfony\Component\Routing\RouteCollection;
@@ -17,15 +19,30 @@ use Symfony\Component\Routing\RouteCollection;
 
 class EdificeTestCase extends \PHPUnit_Framework_TestCase {
 
+	/**
+	 * @var Illuminate\Routing\UrlGenerator
+	 */
 	protected $urlGenerator;
+
+	/**
+	 * @var Illuminate\Html\HtmlBuilder
+	 */
 	protected $htmlBuilder;
+
+	/**
+	 * @var Illuminate\Html\FormBuilder
+	 */
 	protected $formBuilder;
+
+	/**
+	 * @var Lionart\Edifice\Form\EdificeForm
+	 */
 	protected $edifice;
 
 	protected function setUp() {
-		$this->urlGenerator = new UrlGenerator(new RouteCollection, Request::create('/foo', 'GET'));
-		$this->htmlBuilder  = new HtmlBuilder;
+		$this->urlGenerator = new UrlGenerator(new RouteCollection, Request::create('/edifice', 'GET'));
+		$this->htmlBuilder  = new HtmlBuilder($this->urlGenerator);
 		$this->formBuilder  = new FormBuilder($this->htmlBuilder, $this->urlGenerator, 'csrfToken');
-		$this->edifice      = new EdificeForm($this->formBuilder, new Store);
+		$this->edifice      = new EdificeForm($this->formBuilder, new Store());
 	}
 }
