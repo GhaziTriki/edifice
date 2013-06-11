a9cccdd31d0a5092e787df297095d24ab0808b50
Ghazi Triki <ghazi.nocturne@gmail.com>
2013-06-11 10:59:24 +0100

    Updated variables names.

--- tests/unit/EmailInputTest.php
+++ tests/unit/EmailInputTest.php
@@ -17,40 +17,40 @@ class EmailInputTest extends EdificeTestCase {
 	}
 
 	public function testSimpleEmailInput() {
-		$textSimple        = $this->edifice->email('user_email');
-		$textRequiredStyle = $this->edifice->email('user_email', 'ghazi@github.com', array('class' => 'required', 'required' => 'required'));
-		$textPlaceholder   = $this->edifice->email('user_email', null, array('placeholder' => 'First name'));
+		$emailSimple        = $this->edifice->email('user_email');
+		$emailRequiredStyle = $this->edifice->email('user_email', 'ghazi@github.com', array('class' => 'required', 'required' => 'required'));
+		$emailPlaceholder   = $this->edifice->email('user_email', null, array('placeholder' => 'First name'));
 
-		$this->assertEquals('<div class="row"><input name="user_email" type="email"></div>', $textSimple);
-		$this->assertEquals('<div class="row"><input class="required" required="required" name="user_email" type="email" value="ghazi@github.com"></div>', $textRequiredStyle);
-		$this->assertEquals('<div class="row"><input placeholder="First name" name="user_email" type="email"></div>', $textPlaceholder);
+		$this->assertEquals('<div class="row"><input name="user_email" type="email"></div>', $emailSimple);
+		$this->assertEquals('<div class="row"><input class="required" required="required" name="user_email" type="email" value="ghazi@github.com"></div>', $emailRequiredStyle);
+		$this->assertEquals('<div class="row"><input placeholder="First name" name="user_email" type="email"></div>', $emailPlaceholder);
 	}
 
 	public function testEmailWithTopLabel() {
-		$textWithLabelTopLeft  = $this->edifice->email('user_email', null, array('label' => array('text' => 'First name')));
-		$textWithLabelTopRight = $this->edifice->email('user_email', null, array('label' => array('text' => 'First name', 'align' => 'right')));
+		$emailWithLabelTopLeft  = $this->edifice->email('user_email', null, array('label' => array('text' => 'First name')));
+		$emailWithLabelTopRight = $this->edifice->email('user_email', null, array('label' => array('text' => 'First name', 'align' => 'right')));
 
-		$this->assertEquals('<div class="row"><label for="user_email">First name</label><input name="user_email" type="email" id="user_email"></div>', $textWithLabelTopLeft);
-		$this->assertEquals('<div class="row"><label for="user_email" class="right">First name</label><input name="user_email" type="email" id="user_email"></div>', $textWithLabelTopRight);
+		$this->assertEquals('<div class="row"><label for="user_email">First name</label><input name="user_email" type="email" id="user_email"></div>', $emailWithLabelTopLeft);
+		$this->assertEquals('<div class="row"><label for="user_email" class="right">First name</label><input name="user_email" type="email" id="user_email"></div>', $emailWithLabelTopRight);
 	}
 
 	public function testEmailWithInlineLabel() {
-		$textWithLabelInlineLeft  = $this->edifice->email('user_email', null, array('label' => array('text' => 'First name', 'inline' => true)));
-		$textWithLabelInlineRight = $this->edifice->email('user_email', null, array('label' => array('text' => 'First name', 'inline' => true, 'align' => 'right')));
+		$emailWithLabelInlineLeft  = $this->edifice->email('user_email', null, array('label' => array('text' => 'First name', 'inline' => true)));
+		$emailWithLabelInlineRight = $this->edifice->email('user_email', null, array('label' => array('text' => 'First name', 'inline' => true, 'align' => 'right')));
 
-		$this->assertEquals('<div class="row"><div class="small-4 large-4 columns"><label for="user_email" class="inline">First name</label></div><div class="small-8 large-8 columns"><input name="user_email" type="email" id="user_email"></div></div>', $textWithLabelInlineLeft);
-		$this->assertEquals('<div class="row"><div class="small-4 large-4 columns"><label for="user_email" class="inline right">First name</label></div><div class="small-8 large-8 columns"><input name="user_email" type="email" id="user_email"></div></div>', $textWithLabelInlineRight);
+		$this->assertEquals('<div class="row"><div class="small-4 large-4 columns"><label for="user_email" class="inline">First name</label></div><div class="small-8 large-8 columns"><input name="user_email" type="email" id="user_email"></div></div>', $emailWithLabelInlineLeft);
+		$this->assertEquals('<div class="row"><div class="small-4 large-4 columns"><label for="user_email" class="inline right">First name</label></div><div class="small-8 large-8 columns"><input name="user_email" type="email" id="user_email"></div></div>', $emailWithLabelInlineRight);
 	}
 
 	public function testEmailWithPrefix() {
-		$textWithPrefix = $this->edifice->email('user_email', 'github.com', array('prefix' => array('text' => 'domain.')));
+		$emailWithPrefix = $this->edifice->email('user_email', 'github.com', array('prefix' => array('text' => 'domain.')));
 
-		$this->assertEquals('<div class="row collapse"><div class="small-4 large-4 columns"><span class="prefix">domain.</span></div><div class="small-8 large-8 columns"><input name="user_email" type="email" value="github.com"></div></div>', $textWithPrefix);
+		$this->assertEquals('<div class="row collapse"><div class="small-4 large-4 columns"><span class="prefix">domain.</span></div><div class="small-8 large-8 columns"><input name="user_email" type="email" value="github.com"></div></div>', $emailWithPrefix);
 	}
 
 	public function testEmailWithPostfix() {
-		$textWithPrefix = $this->edifice->email('user_email', 'ghazi@github.com', array('postfix' => array('text' => '.tn')));
+		$emailWithPrefix = $this->edifice->email('user_email', 'ghazi@github.com', array('postfix' => array('text' => '.tn')));
 
-		$this->assertEquals('<div class="row collapse"><div class="small-8 large-8 columns"><input name="user_email" type="email" value="ghazi@github.com"></div><div class="small-4 large-4 columns"><span class="postfix">.tn</span></div></div>', $textWithPrefix);
+		$this->assertEquals('<div class="row collapse"><div class="small-8 large-8 columns"><input name="user_email" type="email" value="ghazi@github.com"></div><div class="small-4 large-4 columns"><span class="postfix">.tn</span></div></div>', $emailWithPrefix);
 	}
 }
--- tests/unit/TextareaInputTest.php
+++ tests/unit/TextareaInputTest.php
@@ -12,29 +12,29 @@ class TextareaInputTest extends EdificeTestCase {
 	}
 
 	public function testSimpleTextArea() {
-		$textSimple        = $this->edifice->textarea('user_comments');
-		$textRequiredStyle = $this->edifice->textarea('user_comments', 'Ghazi', array('class' => 'required', 'required' => 'required'));
-		$textPlaceholder   = $this->edifice->textarea('user_comments', null, array('placeholder' => 'User comments'));
+		$textAreaSimple        = $this->edifice->textarea('user_comments');
+		$textAreaRequiredStyle = $this->edifice->textarea('user_comments', 'Ghazi', array('class' => 'required', 'required' => 'required'));
+		$textAreaPlaceholder   = $this->edifice->textarea('user_comments', null, array('placeholder' => 'User comments'));
 
-		$this->assertEquals('<div class="row"><textarea name="user_comments" cols="50" rows="10"></textarea></div>', $textSimple);
-		$this->assertEquals('<div class="row"><textarea class="required" required="required" name="user_comments" cols="50" rows="10">Ghazi</textarea></div>', $textRequiredStyle);
-		$this->assertEquals('<div class="row"><textarea placeholder="User comments" name="user_comments" cols="50" rows="10"></textarea></div>', $textPlaceholder);
+		$this->assertEquals('<div class="row"><textarea name="user_comments" cols="50" rows="10"></textarea></div>', $textAreaSimple);
+		$this->assertEquals('<div class="row"><textarea class="required" required="required" name="user_comments" cols="50" rows="10">Ghazi</textarea></div>', $textAreaRequiredStyle);
+		$this->assertEquals('<div class="row"><textarea placeholder="User comments" name="user_comments" cols="50" rows="10"></textarea></div>', $textAreaPlaceholder);
 	}
 
 	public function testTextAreaWithTopLabel() {
-		$textWithLabelTopLeft  = $this->edifice->textarea('user_comments', null, array('label' => array('text' => 'User comments')));
-		$textWithLabelTopRight = $this->edifice->textarea('user_comments', null, array('label' => array('text' => 'User comments', 'align' => 'right')));
+		$textAreaWithLabelTopLeft  = $this->edifice->textarea('user_comments', null, array('label' => array('text' => 'User comments')));
+		$textAreaWithLabelTopRight = $this->edifice->textarea('user_comments', null, array('label' => array('text' => 'User comments', 'align' => 'right')));
 
-		$this->assertEquals('<div class="row"><label for="user_comments">User comments</label><textarea name="user_comments" cols="50" rows="10" id="user_comments"></textarea></div>', $textWithLabelTopLeft);
-		$this->assertEquals('<div class="row"><label for="user_comments" class="right">User comments</label><textarea name="user_comments" cols="50" rows="10" id="user_comments"></textarea></div>', $textWithLabelTopRight);
+		$this->assertEquals('<div class="row"><label for="user_comments">User comments</label><textarea name="user_comments" cols="50" rows="10" id="user_comments"></textarea></div>', $textAreaWithLabelTopLeft);
+		$this->assertEquals('<div class="row"><label for="user_comments" class="right">User comments</label><textarea name="user_comments" cols="50" rows="10" id="user_comments"></textarea></div>', $textAreaWithLabelTopRight);
 	}
 
 	public function testTextAreaWithInlineLabel() {
-		$textWithLabelInlineLeft  = $this->edifice->textarea('user_comments', null, array('label' => array('text' => 'User comments', 'inline' => true)));
-		$textWithLabelInlineRight = $this->edifice->textarea('user_comments', null, array('label' => array('text' => 'User comments', 'inline' => true, 'align' => 'right')));
+		$textAreaWithLabelInlineLeft  = $this->edifice->textarea('user_comments', null, array('label' => array('text' => 'User comments', 'inline' => true)));
+		$textAreaWithLabelInlineRight = $this->edifice->textarea('user_comments', null, array('label' => array('text' => 'User comments', 'inline' => true, 'align' => 'right')));
 
-		$this->assertEquals('<div class="row"><div class="small-4 large-4 columns"><label for="user_comments" class="inline">User comments</label></div><div class="small-8 large-8 columns"><textarea name="user_comments" cols="50" rows="10" id="user_comments"></textarea></div></div>', $textWithLabelInlineLeft);
-		$this->assertEquals('<div class="row"><div class="small-4 large-4 columns"><label for="user_comments" class="inline right">User comments</label></div><div class="small-8 large-8 columns"><textarea name="user_comments" cols="50" rows="10" id="user_comments"></textarea></div></div>', $textWithLabelInlineRight);
+		$this->assertEquals('<div class="row"><div class="small-4 large-4 columns"><label for="user_comments" class="inline">User comments</label></div><div class="small-8 large-8 columns"><textarea name="user_comments" cols="50" rows="10" id="user_comments"></textarea></div></div>', $textAreaWithLabelInlineLeft);
+		$this->assertEquals('<div class="row"><div class="small-4 large-4 columns"><label for="user_comments" class="inline right">User comments</label></div><div class="small-8 large-8 columns"><textarea name="user_comments" cols="50" rows="10" id="user_comments"></textarea></div></div>', $textAreaWithLabelInlineRight);
 	}
 
 }
