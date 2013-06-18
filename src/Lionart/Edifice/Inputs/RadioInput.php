fe41b275ea76744c1c6bb30d722b6258009e49ea
Ghazi Triki <ghazi.nocturne@gmail.com>
2013-06-18 10:29:28 +0100

    Added RadioInput.

--- src/Lionart/Edifice/Inputs/RadioInput.php
+++ src/Lionart/Edifice/Inputs/RadioInput.php
@@ -21,10 +21,10 @@ namespace Lionart\Edifice\Inputs;
 /**
  * Class RadioInput
  * @version 1.0
- * @since   2013-06-13
+ * @since   2013-06-18
  * @author  Ghazi Triki <ghazi.nocturne@gmail.com>
  * @package Lionart\Edifice\Inputs
  */
 class RadioInput extends BooleanInput {
-
+	protected $render_method = 'radio';
 }
